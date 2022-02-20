b37663167ba5056e16b5923c9698fbcb532a217d
Michael Green <mgreen1980@gmail.com>
2022-02-20 14:09:04 -0500

    2.19 done

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -2,39 +2,6 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import CountryList from './components/CountryList'
 
-const api_key = process.env.REACT_APP_API_KEY
-
-const Weather = ({country}) => {
-  const [weather, setWeather] = useState({})
-
-  const lat = country.capitalInfo.latlng[0]
-  const lng = country.capitalInfo.latlng[1]
-
-  useEffect(() => {
-    axios
-      .get(`http://api.openweathermap.org/data/2.5/weather?units=metric&lat=${lat}&lon=${lng}&appid=${api_key}`)
-      .then(response => setWeather(response.data))
-  }, [lat, lng])
-
-  if (Object.keys(weather).length === 0) {
-    return (
-    <div>
-      Retrieving weather for {country.capital}...
-    </div>)
-  }
-  else { 
-    const info = weather.weather[0]
-    return (
-      <div>
-        <h1>Weather in {country.capital}</h1>
-        <img src={`http://openweathermap.org/img/wn/${info.icon}@2x.png`} alt={info.description}></img><br />
-        temperature: {weather.main.temp}° Celcius<br />
-        wind: {weather.wind.speed} m/s
-      </div>
-    )
-  }
-}
-
 const App = () => {
   const [countries, setCountries] = useState([])
   const [searchString, setSearch] = useState('')
